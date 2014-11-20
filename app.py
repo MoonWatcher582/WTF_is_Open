6fe34e8cb7b735083eb1ac8458a2187b96e06f02
MoonWatcher582 <erbronne@gmail.com>
2014-11-20 16:01:33 -0500

    design phase begin

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+import json
+from pprint import pprint
 
 app = Flask(__name__)
 
@@ -9,24 +11,36 @@ def home():
 
 @app.route('/isOpen')
 def isOpen():
+	#if float(request.args['lat']) == 0  or float(request.args['long']) == 0:
+	#	return render_template('noLocation.html');
+	
+	lat = request.args['lat']
+	_long = request.args['long']
+
+	#Key for server apps (with IP locking)
 	key = 'AIzaSyBj6dkQWl1z-Oa2tr2iaix9Gcul3SuQbqE'
-	location = '40.486678,-74.444414'
+
+	#Key for browser apps (with referers)
+	key = 'AIzaSyCp6JnyzRhefAtgJ7h3w0X6PgemLd7uQmk'
+
+	#location = '40.486678,-74.444414'
+	location = request.args['lat'] + ',' + request.args['long']
 	types = 'bakery|bar|cafe|convenience_store|food|liquor_store|meal_delivery|meal_takeaway|retaurant|shopping_mall'
 	#types = 'food'
 
 	if request.args['option1'] == "prominence":
-		radius = request.args['radius']
+		radius = float(request.args['radius']) * 1609.344
+		radius = str(radius)
 		payload = {'key': key, 'location': location, 'radius': radius, 'opennow': 'true', 'rankby': 'prominence', 'types': types}
 	
 	elif request.args['option1'] == "distance":
 		payload = {'key': key, 'location': location, 'opennow': 'true', 'rankby': 'distance', 'types': types}
 
 	r = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=payload)
-	print(r.url)
 	
 	places = r.json()["results"]
-
-	return render_template('isOpen.html', url=r.url, places=places)
+	
+	return render_template('isOpen.html', places=places, lat=lat, _long=_long)
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', port=4000, debug=True)
