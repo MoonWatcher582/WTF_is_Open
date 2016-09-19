d74d44c40ac8bfb6f9c471d4f1bfd7854db9ed70
Patrick Wu <scorpio7500@gmail.com>
2016-09-18 23:53:41 -0400

    I... forgot to add the server.go file lol

---
+++ server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	//	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+const (
+	HOME        = "templates/index.html"
+	OPEN        = "templates/isOpen.html"
+	NO_LOCATION = "templates/noLocation.html"
+	PORT        = ":8080"
+)
+
+type Message struct {
+	Title string
+	Text  string
+}
+
+var client *http.Client
+
+func renderTemplate(w http.ResponseWriter, r *http.Request, msg *Message, path string) {
+	t, err := template.ParseFiles(path)
+	if err != nil {
+		http.NotFound(w, r)
+	}
+	err = t.Execute(w, msg)
+	if err != nil {
+		fmt.Println("Error writing to response writer\n\b", err)
+	}
+	return
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, r, &Message{Title: "What the Fuck is Open?", Text: "Would you like to sort by prominence or distance?"}, HOME)
+	return
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	fmt.Println("Listening and serving on port", PORT)
+	http.ListenAndServe(PORT, nil)
+	return
+}
